63d07b7301d1161f4b55980d5ee29124d9da87db
Andrei Vladescu <rudolfal2lea@yahoo.com>
2021-10-23 22:50:27 +0300

    Alpha v 1.06
    Fully Working Music Player
    
    Co-Authored-By: razvansuhan <92596462+razvansuhan@users.noreply.github.com>

--- Wargame/Forms/MusicPlayerForm.cs
+++ Wargame/Forms/MusicPlayerForm.cs
@@ -11,23 +11,27 @@ using System.Windows.Forms;
 
 using System.IO;
 using System.Collections;
+using Wargame.User_Defined.Tools;
 
 namespace Wargame.Forms
 {
     public partial class MusicPlayerForm : Form
     {
+        String[] paths, files;
+        public bool True { get; private set; }
+        string propertyList = "";
+        List<int> melodii = new List<int>();
+
         public MusicPlayerForm()
         {
             InitializeComponent();
-            string[] files = System.IO.Directory.GetFiles("E:\\music\\");
+            string[] files = System.IO.Directory.GetFiles(Tools.dirPath + "Resources\\Music\\");
 
             List<string> pathsList = new List<string> { };
 
             for (int i = 0; i < files.Count(); i++)
             {
-                files[i] = files[i].Remove(0, 9);
-                string buff = "E:\\music\\" + files[i];
-                pathsList.Add(buff);
+                pathsList.Add(files[i]);
             }
             this.paths = pathsList.ToArray();
 
@@ -44,22 +48,6 @@ namespace Wargame.Forms
             lbl_volume.Text = trackBar1.Value.ToString() + "%";
             
         }
-
-       
-
-        String[] paths,files;
-        public bool True { get; private set; }
-        string propertyList = "";
-        List<int> melodii = new List<int>();
-
-        //private void btnSelectSongs_Click(object sender, EventArgs e)
-        //{
-
-        //    //for (int i = 0; i < melodii.Count; i++)
-        //    //{
-        //    //    int v = melodii[i];
-        //    //}
-        //}
         private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
         {
 
@@ -69,19 +57,10 @@ namespace Wargame.Forms
         {
 
         }
-
         private void WindowsMediaPlayer_Enter(object sender, EventArgs e)
         {
 
         }
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-        //    WindowsMediaPlayer.Ctlcontrols.play();
-        //    timer1.Start();
-        //    trackBar1.Value = 30;
-        //    lbl_volume.Text = trackBar1.Value.ToString() + "%";
-        //}
-
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (WindowsMediaPlayer.playState == WMPLib.WMPPlayState.wmppsPlaying)
@@ -105,7 +84,6 @@ namespace Wargame.Forms
                 WindowsMediaPlayer.Ctlcontrols.play();
             }
         }
-
         private void btn_play_Click(object sender, EventArgs e)
         {
             WindowsMediaPlayer.Ctlcontrols.play();
@@ -113,17 +91,14 @@ namespace Wargame.Forms
             trackBar1.Value = 30;
             lbl_volume.Text = trackBar1.Value.ToString() + "%";
         }
-
         private void btn_pause_Click(object sender, EventArgs e)
         {
             WindowsMediaPlayer.Ctlcontrols.pause();
         }
-
         private void btn_stop_Click(object sender, EventArgs e)
         {
             WindowsMediaPlayer.Ctlcontrols.stop();
         }
-
         private void btn_prev_Click(object sender, EventArgs e)
         {
             if (listBoxSongs.SelectedIndex > 0)
@@ -131,7 +106,6 @@ namespace Wargame.Forms
                 listBoxSongs.SelectedIndex = listBoxSongs.SelectedIndex - 1;
             }
         }
-
         private void btn_next_Click(object sender, EventArgs e)
         {
             if (listBoxSongs.SelectedIndex < listBoxSongs.Items.Count - 1)
@@ -139,17 +113,15 @@ namespace Wargame.Forms
                 listBoxSongs.SelectedIndex = listBoxSongs.SelectedIndex + 1;
             }
         }
-
         private void btn_sunet_Click(object sender, EventArgs e)
         {
-           WindowsMediaPlayer2.URL = @"E:\Music\DaVasile.mp3";
+           WindowsMediaPlayer2.URL = @Tools.dirPath + @"Resources\\Music\\DaVasile.mp3";
            WindowsMediaPlayer2.Ctlcontrols.play();
         }
         private void trackBar1_Scroll_1(object sender, EventArgs e)
         {
             WindowsMediaPlayer.settings.volume = trackBar1.Value;
             lbl_volume.Text = trackBar1.Value.ToString() + "%";
-
         }
         private void progressBar1_MouseDown(object sender, MouseEventArgs e)
         {
@@ -158,60 +130,45 @@ namespace Wargame.Forms
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-            //this.WindowState = FormWindowState.Minimized;
             this.Visible = false;
         }
-
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            //this.Close();
-        }
 
+        }
         private void WindowsMediaPlayer_Enter_1(object sender, EventArgs e)
         {
 
         }
-
         private void button4_Click(object sender, EventArgs e)
         {
 
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
 
         }
-
         private void lbl_track_start_Click(object sender, EventArgs e)
         {
 
         }
-
         private void lbl_track_end_Click(object sender, EventArgs e)
         {
 
         }
-
         private void progressBar1_Click(object sender, EventArgs e)
         {
 
         }
-
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             WindowsMediaPlayer.settings.volume = trackBar1.Value;
             lbl_volume.Text = trackBar1.Value.ToString() + "%";
         }
-
         private void listBoxSongs_SelectedIndexChanged_1(object sender, EventArgs e)
         {
             WindowsMediaPlayer.URL = paths[listBoxSongs.SelectedIndex];
-            //WindowsMediaPlayer.Ctlcontrols.play();
-            //timer1.Start();
-            //trackBar1.Value = 30;
-            //lbl_volume.Text = trackBar1.Value.ToString() + "%";
         }
-
         private void btn_open_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -226,7 +183,6 @@ namespace Wargame.Forms
                 }
             }
         }
-
         private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
         {
 
